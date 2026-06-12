3256f2b234d4ff6dbf0e5dc36377561c5f9a56ed
ธีรภัทร์ อินทรัตน์ <werasat1212@gmail.com>
2026-06-12 13:48:46 +0700

    input

--- src/TestComputer.java
+++ src/TestComputer.java
@@ -1,18 +1,22 @@
+import java.util.Scanner;
+
 public class TestComputer {
     public static void main(String[] args){
         Computer computer = new Computer();
+        Scanner sc = new Scanner(System.in);
 
-        computer.powerOn();
+        System.out.println("Enther your brand");
+        computer.setBrand(sc.next());
 
-        computer.setBrand("Hp");
-        System.out.println("คอมพิวเตอร์ยี่ห้อ : " + computer.getBrand());
-        computer.setSeries("a67");
-        System.out.println(computer.getSeries());
+        System.out.println("Enther your series");
+        computer.setSeries(sc.next());
+
+
+        System.out.println("Computer brand : " + computer.getBrand());
+        System.out.println("Computer Series : " + computer.getSeries());
         computer.setCpuname("i5 7400f");
         computer.setCpuspeed(5.0f);
         System.out.println(computer.getCpuname() + " " + computer.getCpuspeed() + "GHZ");
 
-        computer.powerOff();
-        computer.restart();
     }
 }
