943567aa50b9460d1f1c378cd1cd216d728dc784
ธีรภัทร์ อินทรัตน์ <werasat1212@gmail.com>
2026-06-12 11:05:50 +0700

    first commit

---
+++ src/Computer.java
@@ -0,0 +1,14 @@
+public class Computer {
+    private String brand;
+    private String series;
+
+    public void PowerOn(){
+        System.out.println("computer is power on");
+    }
+    public void PowerOff(){
+        System.out.println("computer is power off");
+    }
+    public void Restart(){
+        System.out.println("computer is restarting");
+    }
+}
---
+++ src/TestComputer.java
@@ -0,0 +1,9 @@
+public class TestComputer {
+    public static void main(String[] args){
+        Computer computer = new Computer();
+
+        computer.PowerOn();
+        computer.PowerOff();
+        computer.Restart();
+    }
+}
