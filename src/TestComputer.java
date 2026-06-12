94b9b52b6d4df0d34ddfac5f4ca1e585290aff03
ธีรภัทร์ อินทรัตน์ <werasat1212@gmail.com>
2026-06-12 14:14:25 +0700

    cpuname cpuspeed

--- src/TestComputer.java
+++ src/TestComputer.java
@@ -5,17 +5,21 @@ public class TestComputer {
         Computer computer = new Computer();
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enther your brand");
+        System.out.print("Enther your brand : ");
         computer.setBrand(sc.next());
 
-        System.out.println("Enther your series");
+        System.out.print("Enther your series : ");
         computer.setSeries(sc.next());
+
+        System.out.print("Enther your cpuname : ");
+        computer.setCpuname(sc.next());
+
+        System.out.print("Enther your cpuspeed : ");
+        computer.setCpuspeed(sc.nextFloat());
 
 
         System.out.println("Computer brand : " + computer.getBrand());
         System.out.println("Computer Series : " + computer.getSeries());
-        computer.setCpuname("i5 7400f");
-        computer.setCpuspeed(5.0f);
         System.out.println(computer.getCpuname() + " " + computer.getCpuspeed() + "GHZ");
 
     }
